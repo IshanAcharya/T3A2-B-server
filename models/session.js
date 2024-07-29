bd78d55ce8b412249c6a12ff4324e05a8534a788
IshanAcharya <ishanacharya@live.com>
2024-07-29 23:33:36 +1000

    Defined Session Schema for user typing sessions.

--- models/session.js
+++ models/session.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 
-const sessionSchema 
+const sessionSchema = new mongoose.Schema({
+    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    date: { type: Date, default: Date.now },
+    wpm: { type: Number, required: true },
+    accuracy: { type: Number, required: true },
+    errors: { type: Number, required: true },
+    difficulty: { type: String, required: true },
+
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+module.exports = Session;
