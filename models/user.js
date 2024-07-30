b8c52c83724ad9dff54e5026d1e8955d047b5aed
IshanAcharya <ishanacharya@live.com>
2024-07-30 21:02:50 +1000

    Added user schema.

--- models/user.js
+++ models/user.js
@@ -5,5 +5,22 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true },
-})
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.MatchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.export = User;
+
 
