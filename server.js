ef815a59395b48d2f60472aad881349b0ea3f323
IshanAcharya <ishanacharya@live.com>
2024-07-31 18:46:19 +1000

    Added routes and middleware to server setup to create entry point for backend of app.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
-authRoutes
-profileRoutes
-sessionRoutes
+const authRoutes = require('./routes/authRoutes');
+const profileRoutes = require('./routes/profileRoutes');
+const sessionRoutes = require('./routes/sessionRoutes');
 
 dotenv.config();
 
@@ -13,6 +13,13 @@ const app = express();
 connectDB();
 
 // Add middleware
+app.use(express.json());
 
 // Add routes
+app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/sessions', sessionRoutes);
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
