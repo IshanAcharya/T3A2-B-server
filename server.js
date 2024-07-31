f891c88a4339b949ae481b00585e8c529945d6eb
IshanAcharya <ishanacharya@live.com>
2024-07-31 18:16:53 +1000

    Created server.js for server setup.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+authRoutes
+profileRoutes
+sessionRoutes
+
+dotenv.config();
+
+const app = express();
+
+// Connect to the MongoDB database
+connectDB();
+
+// Add middleware
+
+// Add routes
+
