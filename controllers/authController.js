0bd463299cd19313c5a8646486f8bf3e52c87dfb
IshanAcharya <ishanacharya@live.com>
2024-08-01 16:14:55 +1000

    Added registerUser function to handle user registration and token generation logic.

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,3 +4,26 @@ const User = require('../models/user');
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
+
+const registerUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const userExists = await User.findOne({ email});
+
+    if (userExists) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({ email, password });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400).json({ message: 'Invalid user data' });
+    }
+};
+
