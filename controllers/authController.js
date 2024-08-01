1d71b040fe9683517cdba62416a932befb68016c
IshanAcharya <ishanacharya@live.com>
2024-08-01 16:27:54 +1000

    Added loginUser function to handle user authentication logic for user login, and token generation.

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,3 +27,20 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401).json({ message: 'Invalid email or password provided' });
+    }
+};
+
+module.exports = { registerUser, loginUser};
