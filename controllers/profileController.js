3fb79ecb08da2b96e85770945a38ce04bc89e5b7
IshanAcharya <ishanacharya@live.com>
2024-08-01 16:51:33 +1000

    Added updateProfile function which allows users to update their email address and password for their account.

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,13 +5,25 @@ const getProfile = async (req, res) => {
     res.json(user);
 };
 
-const updateProfile = 
+const updateProfile = async (req, res) => {
+    const user = await User.findById(req.user._id);
 
+    if (user) {
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
 
-
-
-
-
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            email: updatedUser.email,
+            token: generateToken(updatedUser._id),
+        })
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+};
 
 
 module.exports = { getProfile, updateProfile };
