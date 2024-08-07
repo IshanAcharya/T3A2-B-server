005cf812879a3d5a71398fbeb323e93f8862ef92
IshanAcharya <ishanacharya@live.com>
2024-08-08 03:31:46 +1000

    Created file to conduct unit test of sessionController.

--- tests/profileControllerUnitTest.test.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-const { updateProfile } = require('../controllers/profileController');
-const User = require('../models/user');
---
+++ tests/sessionControllerUnitTest.test.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const { saveSession } = require('../controllers/sessionController');
+const Session = require('../models/session');
+
